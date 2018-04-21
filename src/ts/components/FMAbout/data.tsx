15706995dece93030e7f8807adaf1fa3ba5355b1
Andrea Baldo <andrixb@gmail.com>
2018-04-21 14:10:31 +0200

    Change people data model

--- src/ts/components/FMAbout/data.tsx
+++ src/ts/components/FMAbout/data.tsx
@@ -2,80 +2,230 @@ export default {
     "people": [{
         "name": "Andrea Baldo",
         "description": [
-            "Date of birth: 1985, June 12th",
-            "Eyes and spectacles: brown and no glasses",
-            "Height: 184 cm",
-            "Main field of action: multimedial stuff / things",
-            "For whom you worked: Accademia di Belle Arti di Napoli, BBC, Rutply, RCS, British Heart Foundation, McDonald’ s, BMW, Three, Disney, Cancer Research UK, amazon, Transport for London, Sainsbury\’ s, Google, Patek Philippe, Virgin",
-            "Analogue or digital: hybrid",
-            "Good at: making up things to make other things up",
-            "Unable to: spend too much time in idle",
-            "Spoken languages: Italian, Neapolitan, English, Spanish, Catalan",
-            "Languages you’ d speak: Arabic"
+            {
+                "label": "Date of birth",
+                "value": "1985, June 12th"
+            },
+            {
+                "label": "Eyes and spectacles",
+                "value": "brown and no glasses"
+            },
+            {
+                "label": "Height",
+                "value": "184 cm"
+            },
+            {
+                "label": "Main field of action",
+                "value": "multimedial stuff / things"
+            },
+            {
+                "label": "For whom you worked",
+                "value": "Accademia di Belle Arti di Napoli, BBC, Rutply, RCS, British Heart Foundation, McDonald’ s, BMW, Three, Disney, Cancer Research UK, amazon, Transport for London, Sainsbury\’ s, Google, Patek Philippe, Virgin"
+            },
+            {
+                "label": "Analogue or digital",
+                "value": "hybrid"
+            },
+            {
+                "label": "Good at",
+                "value": "making up things to make other things up"
+            },
+            {
+                "label": "Unable to",
+                "value": "spend too much time in idle"
+            },
+            {
+                "label": "Spoken languages",
+                "value": "Italian, Neapolitan, English, Spanish, Catalan"
+            },
+            {
+                "label": "Languages you’ d speak",
+                "value": "Arabic"
+            }
         ],
         "picture": "/assets/images/ab_profile.jpg"
     },
     {
         "name": "Claudio Morelli",
         "description": [
-            "Date of birth: 1985, June 12th",
-            "Eyes and spectacles: brown and no glasses",
-            "Height: 184 cm",
-            "Main field of action: multimedial stuff / things",
-            "For whom you worked: Accademia di Belle Arti di Napoli, BBC, Rutply, RCS, British Heart Foundation, McDonald’ s, BMW, Three, Disney, Cancer Research UK, amazon, Transport for London, Sainsbury\’ s, Google, Patek Philippe, Virgin",
-            "Analogue or digital: hybrid",
-            "Good at: making up things to make other things up",
-            "Unable to: spend too much time in idle",
-            "Spoken languages: Italian, Neapolitan, English, Spanish, Catalan",
-            "Languages you’ d speak: Arabic"
+            {
+                "label": "Date of birth",
+                "value": "1985, June 12th"
+            },
+            {
+                "label": "Eyes and spectacles",
+                "value": "brown and no glasses"
+            },
+            {
+                "label": "Height",
+                "value": "184 cm"
+            },
+            {
+                "label": "Main field of action",
+                "value": "multimedial stuff / things"
+            },
+            {
+                "label": "For whom you worked",
+                "value": "Accademia di Belle Arti di Napoli, BBC, Rutply, RCS, British Heart Foundation, McDonald’ s, BMW, Three, Disney, Cancer Research UK, amazon, Transport for London, Sainsbury\’ s, Google, Patek Philippe, Virgin"
+            },
+            {
+                "label": "Analogue or digital",
+                "value": "hybrid"
+            },
+            {
+                "label": "Good at",
+                "value": "making up things to make other things up"
+            },
+            {
+                "label": "Unable to",
+                "value": "spend too much time in idle"
+            },
+            {
+                "label": "Spoken languages",
+                "value": "Italian, Neapolitan, English, Spanish, Catalan"
+            },
+            {
+                "label": "Languages you’ d speak",
+                "value": "Arabic"
+            }
         ],
         "picture": "/assets/images/cm_profile.jpg"
     },
     {
         "name": "Raffaele Vertaldi",
         "description": [
-            "Date of birth: 1985, June 12th",
-            "Eyes and spectacles: brown and no glasses",
-            "Height: 184 cm",
-            "Main field of action: multimedial stuff / things",
-            "For whom you worked: Accademia di Belle Arti di Napoli, BBC, Rutply, RCS, British Heart Foundation, McDonald’ s, BMW, Three, Disney, Cancer Research UK, amazon, Transport for London, Sainsbury\’ s, Google, Patek Philippe, Virgin",
-            "Analogue or digital: hybrid",
-            "Good at: making up things to make other things up",
-            "Unable to: spend too much time in idle",
-            "Spoken languages: Italian, Neapolitan, English, Spanish, Catalan",
-            "Languages you’ d speak: Arabic"
+            {
+                "label": "Date of birth",
+                "value": "1985, June 12th"
+            },
+            {
+                "label": "Eyes and spectacles",
+                "value": "brown and no glasses"
+            },
+            {
+                "label": "Height",
+                "value": "184 cm"
+            },
+            {
+                "label": "Main field of action",
+                "value": "multimedial stuff / things"
+            },
+            {
+                "label": "For whom you worked",
+                "value": "Accademia di Belle Arti di Napoli, BBC, Rutply, RCS, British Heart Foundation, McDonald’ s, BMW, Three, Disney, Cancer Research UK, amazon, Transport for London, Sainsbury\’ s, Google, Patek Philippe, Virgin"
+            },
+            {
+                "label": "Analogue or digital",
+                "value": "hybrid"
+            },
+            {
+                "label": "Good at",
+                "value": "making up things to make other things up"
+            },
+            {
+                "label": "Unable to",
+                "value": "spend too much time in idle"
+            },
+            {
+                "label": "Spoken languages",
+                "value": "Italian, Neapolitan, English, Spanish, Catalan"
+            },
+            {
+                "label": "Languages you’ d speak",
+                "value": "Arabic"
+            }
         ],
         "picture": "/assets/images/rv_profile.jpg"
     },
     {
         "name": "Elena Montobbio",
         "description": [
-            "Date of birth: 1985, June 12th",
-            "Eyes and spectacles: brown and no glasses",
-            "Height: 184 cm",
-            "Main field of action: multimedial stuff / things",
-            "For whom you worked: Accademia di Belle Arti di Napoli, BBC, Rutply, RCS, British Heart Foundation, McDonald’ s, BMW, Three, Disney, Cancer Research UK, amazon, Transport for London, Sainsbury\’ s, Google, Patek Philippe, Virgin",
-            "Analogue or digital: hybrid",
-            "Good at: making up things to make other things up",
-            "Unable to: spend too much time in idle",
-            "Spoken languages: Italian, Neapolitan, English, Spanish, Catalan",
-            "Languages you’ d speak: Arabic"
+            {
+                "label": "Date of birth",
+                "value": "1985, June 12th"
+            },
+            {
+                "label": "Eyes and spectacles",
+                "value": "brown and no glasses"
+            },
+            {
+                "label": "Height",
+                "value": "184 cm"
+            },
+            {
+                "label": "Main field of action",
+                "value": "multimedial stuff / things"
+            },
+            {
+                "label": "For whom you worked",
+                "value": "Accademia di Belle Arti di Napoli, BBC, Rutply, RCS, British Heart Foundation, McDonald’ s, BMW, Three, Disney, Cancer Research UK, amazon, Transport for London, Sainsbury\’ s, Google, Patek Philippe, Virgin"
+            },
+            {
+                "label": "Analogue or digital",
+                "value": "hybrid"
+            },
+            {
+                "label": "Good at",
+                "value": "making up things to make other things up"
+            },
+            {
+                "label": "Unable to",
+                "value": "spend too much time in idle"
+            },
+            {
+                "label": "Spoken languages",
+                "value": "Italian, Neapolitan, English, Spanish, Catalan"
+            },
+            {
+                "label": "Languages you’ d speak",
+                "value": "Arabic"
+            }
         ],
         "picture": "/assets/images/em_profile.jpg"
     },
     {
         "name": "Gianluca Panella",
         "description": [
-            "Date of birth: 1985, June 12th",
-            "Eyes and spectacles: brown and no glasses",
-            "Height: 184 cm",
-            "Main field of action: multimedial stuff / things",
-            "For whom you worked: Accademia di Belle Arti di Napoli, BBC, Rutply, RCS, British Heart Foundation, McDonald’ s, BMW, Three, Disney, Cancer Research UK, amazon, Transport for London, Sainsbury\’ s, Google, Patek Philippe, Virgin",
-            "Analogue or digital: hybrid",
-            "Good at: making up things to make other things up",
-            "Unable to: spend too much time in idle",
-            "Spoken languages: Italian, Neapolitan, English, Spanish, Catalan",
-            "Languages you’ d speak: Arabic"
+            {
+                "label": "Date of birth",
+                "value": "1985, June 12th"
+            },
+            {
+                "label": "Eyes and spectacles",
+                "value": "brown and no glasses"
+            },
+            {
+                "label": "Height",
+                "value": "184 cm"
+            },
+            {
+                "label": "Main field of action",
+                "value": "multimedial stuff / things"
+            },
+            {
+                "label": "For whom you worked",
+                "value": "Accademia di Belle Arti di Napoli, BBC, Rutply, RCS, British Heart Foundation, McDonald’ s, BMW, Three, Disney, Cancer Research UK, amazon, Transport for London, Sainsbury\’ s, Google, Patek Philippe, Virgin"
+            },
+            {
+                "label": "Analogue or digital",
+                "value": "hybrid"
+            },
+            {
+                "label": "Good at",
+                "value": "making up things to make other things up"
+            },
+            {
+                "label": "Unable to",
+                "value": "spend too much time in idle"
+            },
+            {
+                "label": "Spoken languages",
+                "value": "Italian, Neapolitan, English, Spanish, Catalan"
+            },
+            {
+                "label": "Languages you’ d speak",
+                "value": "Arabic"
+            }
         ],
         "picture": "/assets/images/gp_profile.jpg"
     }
