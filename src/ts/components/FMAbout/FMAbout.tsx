d5d0086fc760962c5c3bd0ee4319eac60e98774a
Andrea Baldo <andrixb@gmail.com>
2018-04-22 12:50:26 +0200

    Add darkener background

--- src/ts/components/FMAbout/FMAbout.tsx
+++ src/ts/components/FMAbout/FMAbout.tsx
@@ -6,29 +6,35 @@ import { FMLogo } from './FMLogo';
 
 import './FMAbout.scss';
 
-export interface FMAboutProps {}
+export interface FMAboutProps { }
 
 export class FMAbout extends React.Component<FMAboutProps, any> {
     constructor(props: FMAboutProps) {
         super(props);
-        this.state = { 
-            logoClicked: false 
+        this.state = {
+            logoClicked: false,
+            fade: false,
         };
         this.handleChange.bind(this);
     }
 
     private handleChange(event: React.MouseEvent<Event>): void {
         this.setState({
-            logoClicked: !this.state.logoClicked
+            logoClicked: !this.state.logoClicked,
+            fade: !this.state.darken,
         });
     };
 
     render() {
         return (
-            <div className="about__container" onClick={this.handleChange.bind(this)}>
-                <FMNavigation expand={this.state.logoClicked} />
-                <FMLogo logo="fedia" />
-                <FMPerson />
+            <div className={`${this.state.fade ? 'darken' : ''}`}>
+                <div className="about__logo" onClick={this.handleChange.bind(this)}>
+                    <FMNavigation expand={this.state.logoClicked} />
+                    <FMLogo logo="fedia" />
+                </div>
+                <div className="about__container">
+                    <FMPerson />
+                </div>
             </div>
         );
     }
