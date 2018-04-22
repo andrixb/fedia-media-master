6dbb1a94d3d7f958596caa3e0ab4eb5ee1b094fb
Andrea Baldo <andrixb@gmail.com>
2018-04-22 11:27:21 +0200

    Add data

--- src/ts/components/FMAbout/data.tsx
+++ src/ts/components/FMAbout/data.tsx
@@ -8,7 +8,7 @@ export default {
             },
             {
                 "label": "Eyes and spectacles",
-                "value": "brown and no glasses"
+                "value": "Brown and no glasses"
             },
             {
                 "label": "Height",
@@ -16,7 +16,7 @@ export default {
             },
             {
                 "label": "Main field of action",
-                "value": "multimedial stuff / things"
+                "value": "Multimedial stuff / things"
             },
             {
                 "label": "For whom you worked",
@@ -24,15 +24,15 @@ export default {
             },
             {
                 "label": "Analogue or digital",
-                "value": "hybrid"
+                "value": "Hybrid"
             },
             {
                 "label": "Good at",
-                "value": "making up things to make other things up"
+                "value": "Making up things to make other things up"
             },
             {
                 "label": "Unable to",
-                "value": "spend too much time in idle"
+                "value": "Spend too much time in idle"
             },
             {
                 "label": "Spoken languages",
@@ -41,6 +41,34 @@ export default {
             {
                 "label": "Languages you’ d speak",
                 "value": "Arabic"
+            },
+            {
+                "label": "Lived places",
+                "value": "Naples (I), London (UK), Barcelona (ES)"
+            },
+            {
+                "label": "Movings made",
+                "value": "too many"
+            },
+            {
+                "label": "Favourite cartoon",
+                "value": "Alice in Wonderland"
+            },
+            {
+                "label": "Colour you’d not wear at all",
+                "value": "pink, in all of its shades"
+            },
+            {
+                "label": "Vehicle",
+                "value": "bike"
+            },
+            {
+                "label": "Always with",
+                "value": "fresh ideas"
+            },
+            {
+                "label": "Never without",
+                "value": "music"
             }
         ],
         "picture": "/assets/images/ab_profile.jpg"
@@ -50,181 +78,325 @@ export default {
         "description": [
             {
                 "label": "Date of birth",
-                "value": "1985, June 12th"
+                "value": "1985, July 30th"
             },
             {
                 "label": "Eyes and spectacles",
-                "value": "brown and no glasses"
+                "value": "I am sure I've lost my fantastic round glasses on Panella's boat"
             },
             {
                 "label": "Height",
-                "value": "184 cm"
+                "value": "182 cm"
             },
             {
                 "label": "Main field of action",
-                "value": "multimedial stuff / things"
+                "value": "Volcanoes"
             },
             {
                 "label": "For whom you worked",
-                "value": "Accademia di Belle Arti di Napoli, BBC, Rutply, RCS, British Heart Foundation, McDonald’ s, BMW, Three, Disney, Cancer Research UK, amazon, Transport for London, Sainsbury\’ s, Google, Patek Philippe, Virgin"
+                "value": "IL magazine (Il Sole 24 ORE), RCS, Mondadori, Renzo Piano G124, ADAC Reisemagazine, National Geographic Traveler, Fendi, Nestlè Purina, Focus Magazin, Woz, Monocle, Libèration, FAI Fondo Ambiente Italiano"
             },
             {
                 "label": "Analogue or digital",
-                "value": "hybrid"
+                "value": "WTF?!"
             },
             {
                 "label": "Good at",
-                "value": "making up things to make other things up"
+                "value": "drill, hammer, level, pressure cooker, electoral systems, Moleskine plain notebooks, cameras with few automatisms, photographic enlargers, multilayer pipes, lamps, pumping"
             },
             {
                 "label": "Unable to",
-                "value": "spend too much time in idle"
+                "value": "ski and everything related to this boring stuff on mountains (except volcanoes)"
             },
             {
                 "label": "Spoken languages",
-                "value": "Italian, Neapolitan, English, Spanish, Catalan"
+                "value": "whole Italian, tv series English, reggaeton Spanish and impolite Neapolitan"
             },
             {
                 "label": "Languages you’ d speak",
                 "value": "Arabic"
+            },
+            {
+                "label": "Lived places",
+                "value": "Naples (I), London (UK), Barcelona (ES)"
+            },
+            {
+                "label": "Movings made",
+                "value": "too many"
+            },
+            {
+                "label": "Favourite cartoon",
+                "value": "Alice in Wonderland"
+            },
+            {
+                "label": "Colour you’d not wear at all",
+                "value": "pink, in all of its shades"
+            },
+            {
+                "label": "Vehicle",
+                "value": "bike"
+            },
+            {
+                "label": "Always with",
+                "value": "fresh ideas"
+            },
+            {
+                "label": "Never without",
+                "value": "music"
             }
         ],
         "picture": "/assets/images/cm_profile.jpg"
-    },
-    {
-        "name": "Raffaele Vertaldi",
-        "description": [
-            {
-                "label": "Date of birth",
-                "value": "1985, June 12th"
-            },
-            {
-                "label": "Eyes and spectacles",
-                "value": "brown and no glasses"
-            },
-            {
-                "label": "Height",
-                "value": "184 cm"
-            },
-            {
-                "label": "Main field of action",
-                "value": "multimedial stuff / things"
-            },
-            {
-                "label": "For whom you worked",
-                "value": "Accademia di Belle Arti di Napoli, BBC, Rutply, RCS, British Heart Foundation, McDonald’ s, BMW, Three, Disney, Cancer Research UK, amazon, Transport for London, Sainsbury\’ s, Google, Patek Philippe, Virgin"
-            },
-            {
-                "label": "Analogue or digital",
-                "value": "hybrid"
-            },
-            {
-                "label": "Good at",
-                "value": "making up things to make other things up"
-            },
-            {
-                "label": "Unable to",
-                "value": "spend too much time in idle"
-            },
-            {
-                "label": "Spoken languages",
-                "value": "Italian, Neapolitan, English, Spanish, Catalan"
-            },
-            {
-                "label": "Languages you’ d speak",
-                "value": "Arabic"
-            }
-        ],
-        "picture": "/assets/images/rv_profile.jpg"
     },
     {
         "name": "Elena Montobbio",
         "description": [
             {
                 "label": "Date of birth",
-                "value": "1985, June 12th"
+                "value": "1980, 210 days before the end of the year"
             },
             {
                 "label": "Eyes and spectacles",
-                "value": "brown and no glasses"
+                "value": "brown, never without glasses"
             },
             {
                 "label": "Height",
-                "value": "184 cm"
+                "value": "I don’t need spike heels"
             },
             {
                 "label": "Main field of action",
-                "value": "multimedial stuff / things"
+                "value": "journalism, publishing, editorial design, data informations"
             },
             {
                 "label": "For whom you worked",
-                "value": "Accademia di Belle Arti di Napoli, BBC, Rutply, RCS, British Heart Foundation, McDonald’ s, BMW, Three, Disney, Cancer Research UK, amazon, Transport for London, Sainsbury\’ s, Google, Patek Philippe, Virgin"
+                "value": "Cases y Associats (Buenos Aires), Left Avvenimenti, Valeri Srl (redesign national daily newspaper, content, infographics), SNDE, IL magazine (Il Sole 24 ORE), Panorama, some local newspapers and magazines, Class editori, Swan Group, Aic, Il Corriere del Ticino (Svizzera)"
             },
             {
                 "label": "Analogue or digital",
-                "value": "hybrid"
+                "value": "still looking for my religion"
             },
             {
                 "label": "Good at",
-                "value": "making up things to make other things up"
+                "value": "Indesign, Photoshop, Illustrator"
             },
             {
                 "label": "Unable to",
-                "value": "spend too much time in idle"
+                "value": "feel bored"
             },
             {
                 "label": "Spoken languages",
-                "value": "Italian, Neapolitan, English, Spanish, Catalan"
+                "value": "Italian, Spanish, English, Brazilian Portuguese (same same, but different)"
+            },
+            {
+                "label": "Languages you’ d speak",
+                "value": "mother tongue English, Arabic"
+            },
+            {
+                "label": "Lived places",
+                "value": "Naples (I), London (UK), Barcelona (ES)"
+            },
+            {
+                "label": "Movings made",
+                "value": "too many"
+            },
+            {
+                "label": "Favourite cartoon",
+                "value": "Alice in Wonderland"
+            },
+            {
+                "label": "Colour you’d not wear at all",
+                "value": "pink, in all of its shades"
+            },
+            {
+                "label": "Vehicle",
+                "value": "bike"
+            },
+            {
+                "label": "Always with",
+                "value": "fresh ideas"
+            },
+            {
+                "label": "Never without",
+                "value": "music"
             },
             {
                 "label": "Languages you’ d speak",
                 "value": "Arabic"
+            },
+            {
+                "label": "Lived places",
+                "value": "Naples (I), London (UK), Barcelona (ES)"
+            },
+            {
+                "label": "Movings made",
+                "value": "too many"
+            },
+            {
+                "label": "Favourite cartoon",
+                "value": "Alice in Wonderland"
+            },
+            {
+                "label": "Colour you’d not wear at all",
+                "value": "pink, in all of its shades"
+            },
+            {
+                "label": "Vehicle",
+                "value": "bike"
+            },
+            {
+                "label": "Always with",
+                "value": "fresh ideas"
+            },
+            {
+                "label": "Never without",
+                "value": "music"
             }
         ],
         "picture": "/assets/images/em_profile.jpg"
+    },
+    {
+        "name": "Raffaele Vertaldi",
+        "description": [
+            {
+                "label": "Date of birth",
+                "value": "same day as Indiana Jones, just 79 years later"
+            },
+            {
+                "label": "Eyes and spectacles",
+                "value": "figured mahogany"
+            },
+            {
+                "label": "Height",
+                "value": "same as it ever was"
+            },
+            {
+                "label": "Main field of action",
+                "value": "vision"
+            },
+            {
+                "label": "For whom you worked",
+                "value": "IL magazine, How To Spend It (Il Sole 24 ORE), C.F.P. Riccardo Bauer, GRIN (Gruppo Redattori Iconografici Nazionale), Fondazione Modena Fotografia, TOMO TOMO, Cortona ON THE MOVE, Novella 2000, Visto, OK Salute (RCS), Famiglia38Fotografi, Microsoft Italia"
+            },
+            {
+                "label": "Analogue or digital",
+                "value": "analogous"
+            },
+            {
+                "label": "Good at",
+                "value": "talk and listen"
+            },
+            {
+                "label": "Unable to",
+                "value": "run, even if I’m in a hurry"
+            },
+            {
+                "label": "Spoken languages",
+                "value": "whole Italian, pickled English, sprinkle of Spanish"
+            },
+            {
+                "label": "Languages you’ d speak",
+                "value": "Japanese / Oceanic"
+            },
+            {
+                "label": "Lived places",
+                "value": "Naples (I), London (UK), Barcelona (ES)"
+            },
+            {
+                "label": "Movings made",
+                "value": "too many"
+            },
+            {
+                "label": "Favourite cartoon",
+                "value": "Alice in Wonderland"
+            },
+            {
+                "label": "Colour you’d not wear at all",
+                "value": "pink, in all of its shades"
+            },
+            {
+                "label": "Vehicle",
+                "value": "bike"
+            },
+            {
+                "label": "Always with",
+                "value": "fresh ideas"
+            },
+            {
+                "label": "Never without",
+                "value": "music"
+            }
+        ],
+        "picture": "/assets/images/rv_profile.jpg"
     },
     {
         "name": "Gianluca Panella",
         "description": [
             {
                 "label": "Date of birth",
-                "value": "1985, June 12th"
+                "value": "1976, September 11th"
             },
             {
                 "label": "Eyes and spectacles",
-                "value": "brown and no glasses"
+                "value": "brown - sunglasses"
             },
             {
                 "label": "Height",
-                "value": "184 cm"
+                "value": "180 cm"
             },
             {
                 "label": "Main field of action",
-                "value": "multimedial stuff / things"
+                "value": "photojournalism in crisis areas"
             },
             {
                 "label": "For whom you worked",
-                "value": "Accademia di Belle Arti di Napoli, BBC, Rutply, RCS, British Heart Foundation, McDonald’ s, BMW, Three, Disney, Cancer Research UK, amazon, Transport for London, Sainsbury\’ s, Google, Patek Philippe, Virgin"
+                "value": "Newsweek, Die Zeit, Time, Microsoft, Keystone, Microsoft Skype, The Guardian, Observer, Marie Claire, Espresso, Rcs, Mondadori, International Business Time, Transterra Media"
             },
             {
                 "label": "Analogue or digital",
-                "value": "hybrid"
+                "value": "thinking analog, working digital"
             },
             {
                 "label": "Good at",
-                "value": "making up things to make other things up"
+                "value": "cameras&sails"
             },
             {
                 "label": "Unable to",
-                "value": "spend too much time in idle"
+                "value": "play music"
             },
             {
                 "label": "Spoken languages",
-                "value": "Italian, Neapolitan, English, Spanish, Catalan"
+                "value": "Italian, English"
             },
             {
                 "label": "Languages you’ d speak",
-                "value": "Arabic"
+                "value": "Arabic, Spanish, French, Russian"
+            },
+            {
+                "label": "Lived places",
+                "value": "Naples (I), London (UK), Barcelona (ES)"
+            },
+            {
+                "label": "Movings made",
+                "value": "too many"
+            },
+            {
+                "label": "Favourite cartoon",
+                "value": "Alice in Wonderland"
+            },
+            {
+                "label": "Colour you’d not wear at all",
+                "value": "pink, in all of its shades"
+            },
+            {
+                "label": "Vehicle",
+                "value": "bike"
+            },
+            {
+                "label": "Always with",
+                "value": "fresh ideas"
+            },
+            {
+                "label": "Never without",
+                "value": "music"
             }
         ],
         "picture": "/assets/images/gp_profile.jpg"
